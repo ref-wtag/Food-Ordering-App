39ba7939b11d7de5b68f27c9c0e8f07263ee741c
Refat E Ferdous <reafat.fardous@welldev.io>
2024-09-23 13:43:31 +0600

    added login page

--- app/src/main/java/com/example/foodorderingapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodorderingapp/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.example.foodorderingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.foodorderingapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.dontHaveButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
