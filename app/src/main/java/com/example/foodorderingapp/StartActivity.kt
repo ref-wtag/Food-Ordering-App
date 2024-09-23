958376ce92e9c6b95c80ba93c469dc66296cedd4
Refat E Ferdous <reafat.fardous@welldev.io>
2024-09-23 12:58:22 +0600

    added sign up page

--- app/src/main/java/com/example/foodorderingapp/StartActivity.kt
+++ app/src/main/java/com/example/foodorderingapp/StartActivity.kt
@@ -1,11 +1,23 @@
 package com.example.foodorderingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.foodorderingapp.databinding.ActivitySplashScreenBinding
+import com.example.foodorderingapp.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private val binding: ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(binding.root)
+
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
