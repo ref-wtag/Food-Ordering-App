4674b311def4f82790a736f520c0cb90f58f4e4f
Refat E Ferdous <reafat.fardous@welldev.io>
2024-09-24 15:07:17 +0600

    implemented bottom navigation and fragments

---
+++ app/src/main/java/com/example/foodorderingapp/Fragment/CartFragment.kt
@@ -0,0 +1,28 @@
+package com.example.foodorderingapp.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.foodorderingapp.R
+
+class CartFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+
+    companion object {
+
+    }
+}
